2c5a77ea0b1e90734876268a96753e45eee75866
ItsJonQ <itsjonq@gmail.com>
2018-07-09 09:10:15 -0400

    Adjust ESLint to use React App ESLint config
    Also installs a specific version of Jest + Jest CLI

--- eslint.js
+++ eslint.js
@@ -1,17 +1,21 @@
 const reactLint = require('eslint-config-react-app')
-const kcdLint = require('kcd-scripts/eslint')
 
-const baseConfig = Object.assign({}, kcdLint, reactLint)
+// const kcdLint = require('kcd-scripts/eslint')
 
-const rulesConfig = {
-  'import/prefer-default-export': 'off',
-  'import/no-unassigned-import': 'off',
-  'import/extensions': 'off',
-  'no-console': 'off',
-  'no-nested-ternary': 'off',
-  complexity: ['error', 10],
-}
+// const baseConfig = Object.assign({}, kcdLint, reactLint)
 
-module.exports = Object.assign(baseConfig, {
-  rules: Object.assign(rulesConfig, baseConfig.rules),
-})
+// const rulesConfig = {
+//   'import/prefer-default-export': 'off',
+//   'import/no-unassigned-import': 'off',
+//   'import/extensions': 'off',
+//   'no-console': 'off',
+//   'no-nested-ternary': 'off',
+//   complexity: ['error', 10],
+// }
+
+// const enhancedLint = Object.assign(baseConfig, {
+//   rules: Object.assign(rulesConfig, baseConfig.rules),
+// })
+
+// Going with out-of-the-box react-app Linting config for now.
+export default reactLint
