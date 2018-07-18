6c192ea8d00af17fa2f267ed93f2cd10e13d1745
ItsJonQ <itsjonq@gmail.com>
2018-07-18 15:02:55 -0400

    Babel: Use `babel-preset-react-app`.
    This update updates Babel to use `preset-react-app` by default.
    It also forces `BABEL_ENV=production` if build is triggered.

--- babel.js
+++ babel.js
@@ -1,7 +1,7 @@
 const kcdBabel = require('kcd-scripts/babel')
 
 module.exports = {
-  presets: kcdBabel.presets.concat(require.resolve('babel-preset-flow')),
+  presets: kcdBabel.presets.concat(require.resolve('babel-preset-react-app')),
   plugins: kcdBabel.plugins.concat(
     require.resolve('babel-plugin-transform-flow-strip-types'),
     require.resolve('babel-plugin-inline-svg')
