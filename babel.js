5b792a6f735b525ff9fb71770d7b16b5f4e29768
Jon Quach <hello@jonquach.com>
2018-12-14 16:47:16 -0500

    Babel: Remove react app (#8)
    * Remove babel-react-app
    
    * Add local runtime
    
    * Adjust babelrc
    
    * Adjust build script
    
    * 1.0.3-0
    
    * Downgrade kcd-scripts. Add babel-preset-react-app
    
    * 1.0.3-1
    
    * Latest KCD + no babel-preset-react-app
    
    * 1.0.3-2

--- babel.js
+++ babel.js
@@ -2,7 +2,7 @@ const kcdBabel = require('./dist/config/babelrc')()
 
 module.exports = function zeroBabel() {
   return {
-    presets: kcdBabel.presets.concat(require.resolve('babel-preset-react-app')),
+    presets: kcdBabel.presets,
     plugins: kcdBabel.plugins.concat(
       require.resolve('@babel/plugin-transform-flow-strip-types'),
       require.resolve('babel-plugin-inline-svg'),
