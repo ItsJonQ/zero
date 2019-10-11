357f89c722fffcc36e793ed25a756a86b7475af4
Jon Q <hello@jonquach.com>
2019-10-11 08:11:27 -0400

    Support .prettierrc.js file

--- src/scripts/format.js
+++ src/scripts/format.js
@@ -12,6 +12,7 @@ const hereRelative = p => here(p).replace(process.cwd(), '.')
 const useBuiltinConfig =
   !args.includes('--config') &&
   !hasFile('.prettierrc') &&
+  !hasFile('.prettierrc.js') &&
   !hasFile('prettier.config.js') &&
   !hasPkgProp('prettierrc')
 const config = useBuiltinConfig
