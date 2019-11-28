14e2a183482a2976371675bf2fb0d82870931639
Jon Q <hello@jonquach.com>
2019-11-28 11:37:24 -0500

    Update eslint

--- src/config/eslintrc.js
+++ src/config/eslintrc.js
@@ -2,7 +2,21 @@ const { ifAnyDep } = require('../utils');
 
 module.exports = {
 	extends: [
+		'eslint:recommended',
+		'eslint-config-kentcdodds/jest',
 		ifAnyDep('react', require.resolve('eslint-config-react-app')),
 	].filter(Boolean),
+	parser: 'babel-eslint',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+			modules: true,
+		},
+	},
+	globals: {
+		console: true,
+		global: true,
+		module: true,
+	},
 	rules: {},
 };
