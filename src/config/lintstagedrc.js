ea61376da658aefc3566496b1cb3a94c372b0d89
Jon Q <hello@jonquach.com>
2020-05-02 22:26:58 -0400

    Fix resolve script in lintstagedrc

--- src/config/lintstagedrc.js
+++ src/config/lintstagedrc.js
@@ -1,14 +1,14 @@
-const { resolveKcdScripts, resolveBin, isOptedOut } = require('../utils');
+const { resolveZeroScripts, resolveBin, isOptedOut } = require('../utils');
 
-const kcdScripts = resolveKcdScripts();
+const zeroScripts = resolveZeroScripts();
 const doctoc = resolveBin('doctoc');
 
 module.exports = {
 	'README.md': [`${doctoc} --maxlevel 3 --notitle`, 'git add'],
 	'*.+(js|jsx|json|yml|yaml|css|less|scss|ts|tsx|md|graphql|mdx|vue)': [
-		isOptedOut('autoformat', null, `${kcdScripts} format`),
-		`${kcdScripts} lint`,
-		`${kcdScripts} test --findRelatedTests`,
+		isOptedOut('autoformat', null, `${zeroScripts} format`),
+		`${zeroScripts} lint`,
+		`${zeroScripts} test --findRelatedTests`,
 		isOptedOut('autoformat', null, 'git add'),
 	].filter(Boolean),
 };
